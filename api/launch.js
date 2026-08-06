aadae6d1506803f5099a8024590b528caadeceb8
nugasnugis <nugiefp@gmail.com>
2026-08-06 19:53:24 +0700

    Update launch.js

--- api/launch.js
+++ api/launch.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
 
   try {
-    const response = await fetch('https://github.com', {
+    const response = await fetch('https://api.github.com/repos/nugasnugis/axtest-Web/dispatches', {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${process.env.GH_PAT}`,
